3b215fea5add9d143e047b6bf4cf60c8d7e19970
BudacaB <budaca.b@gmail.com>
2021-02-05 01:28:19 +0200

    rule 1 and 2

--- bigO/src/main/kotlin/main.kt
+++ bigO/src/main/kotlin/main.kt
@@ -1,25 +1,60 @@
+import kotlin.math.floor
 import kotlin.system.measureNanoTime
 
 
 fun main(args: Array<String>) {
-    val nemo = arrayOf("nemo", "bla")
+//    val nemo = arrayOf("nemo", "bla")
+//
+//    val large = Array(5000){"$it"}
+//    large.fill("nemo")
+//
+//    fun findNemo(array: Array<String>) {
+//        for (element in array) {
+//            if (element == "nemo") {
+//                println("Found NEMO!")
+//                break;
+//            }
+//        }
+//    }
+//
+//    val time = measureNanoTime{
+//        findNemo(large) // O(n) -> Linear time
+//    }
+//    println(time)
 
-    val large = Array(5000){"$it"}
-    large.fill("nemo")
+    // Rule 1 -  can't assume "nemo" would be first in the array so we'd get O(1)
+    // Worst case is O(n) if "nemo" is last in the array
 
-    fun findNemo(array: Array<String>) {
-        for (element in array) {
-            if (element == "nemo") {
-                println("Found NEMO!")
-                break;
-            }
-        }
+// -------
+
+//    fun printFirstItemThenFirstHalfThenSayHi100Times(items: Array<String>) {
+//        println(items[0]) // O(1)
+//
+//        var middleIndex = floor((items.size / 2).toDouble())
+//        var index = 0
+//
+//        while (index < middleIndex) { // O(n / 2)
+//            println(items[index])
+//            index++
+//        }
+//
+//        for(i in 0..100) { // O(100)
+//            println("hi")
+//        }
+//    }
+
+    // overall it's O(n)
+    
+
+    fun compressBoxesTwice(boxes: Array<String>) {
+        boxes.forEach { box -> println(box) }
+
+        boxes.forEach { box -> println(box) }
     }
 
-    val time = measureNanoTime{
-        findNemo(large) // O(n) -> Linear time
-    }
-    println(time)
+    // O(2n) => O(n)
+
+// -------
 
 //    val boxes = Array(6){"$it"}
 //    // O(1) -> constant time
